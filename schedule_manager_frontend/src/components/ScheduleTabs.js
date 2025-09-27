17caefe779ece1542fa14f34ef88592492b9715a
Kavia AI <production@kavia.ai>
2025-09-27 15:13:33 +0000

    CheckPoint - cg3b863f34

--- schedule_manager_frontend/src/components/ScheduleTabs.js
+++ schedule_manager_frontend/src/components/ScheduleTabs.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { api } from "../services/api";
 import ICUCalendar from "./ICUCalendar";
-import NowAvailable from "./NowAvailable";
 
 /**
  * PUBLIC_INTERFACE
@@ -32,9 +31,6 @@ export default function ScheduleTabs() {
           </button>
         </div>
       </div>
-
-      {/* Prominent snapshot of what's available right now */}
-      <NowAvailable placement="above" />
 
       <div style={{ padding: 0, overflow: "hidden" }}>
         {activeTab === "availability" ? <ManageAvailability /> : <ICUCalendar />}
